de23a8b7be7ec5d1cbaf79dfd35a1a6b204c0a3e
Sobia <Sobia@MALIK.Home>
2015-10-29 13:47:23 +0500

    leave link added in dashboard

--- dashboard.php
+++ dashboard.php
@@ -12,28 +12,30 @@ include('lib/header.php');
         </li>
     </ul>
 </div>
-<div class=" row">
-    <div class="col-md-3 col-sm-3 col-xs-6">
-        <a data-toggle="tooltip" title="6 new members." class="well top-block" href="#">
-            <i class="glyphicon glyphicon-user blue"></i>
-
-            <div>Total Employees</div>
 <?php  
 
       $obj=new Queries();
       $employee_list=$obj->select("alpp_emp","1 ",array("count(emp_id)"));
-      //var_dump($employee_list);
-   ?>
+      $leave_list=$obj->select("alpp_leave","1 ",array("count(leave_id)"));
+   
+?>
+<div class=" row">
+    <div class="col-md-3 col-sm-3 col-xs-6">
+        <a data-toggle="tooltip" title="<?php echo $employee_list[0][0]; ?> new employees." class="well top-block" href="#">
+            <i class="glyphicon glyphicon-user blue"></i>
+
+            <div>Total Employees</div>
+
             <div><?php echo $employee_list[0][0]; ?></div>
         </a>
     </div>
 
     <div class="col-md-3 col-sm-3 col-xs-6">
-        <a data-toggle="tooltip" title="4 new pro members." class="well top-block" href="#">
+        <a data-toggle="tooltip" title="<?php echo $leave_list[0][0]; ?>." class="well top-block" href="#">
             <i class="glyphicon glyphicon-star green"></i>
 
             <div>Leave Notification</div>
-            <div>0</div>
+            <div><?php echo $leave_list[0][0]; ?></div>
         </a>
     </div>
 
