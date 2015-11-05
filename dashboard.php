91f2ebf391a69a98fdacf91380eda9d01df89d3c
Sobia <Sobia@MALIK.Home>
2015-11-05 11:22:39 +0500

    dashboard leave link

--- dashboard.php
+++ dashboard.php
@@ -8,8 +8,13 @@ include('lib/header.php');
  $employee_list=$obj->select("alpp_emp","1 ",array("count(emp_id)"));
  $leave_list=$obj->select("alpp_leave","1 ",array("count(leave_id)"));
  $balance = $objTransaction->GetEmpBalance($_SESSION['session_admin_id']);
- $leaves_applied=$obj->select("alpp_leave","leave_emp_id=".$_SESSION['session_admin_id'],array("count(leave_id)"));
-      ?>
+ 
+ /// to get the leaves of current month
+ $last_day = date("Y-m-t", strtotime(date('Y-m-01')) ) ;
+ $leaves_applied=$obj->select("alpp_leave","leave_emp_id=".$_SESSION['session_admin_id']." and  "
+         . "( leave_duration_from  between '".date('Y-m-01')."' and '".$last_day."' || leave_duration_to  between '".date('Y-m-01')."' and '".$last_day."') " ,array("count(leave_id)"));
+ 
+ ?>
 <div>
     <ul class="breadcrumb">
         <li>
@@ -69,7 +74,13 @@ include('lib/header.php');
         <a data-toggle="tooltip" title="<?php echo $leaves_applied[0][0]; ?> Applied." class="well top-block" href="#">
             <i class="glyphicon glyphicon-star green"></i>
 
-            <div>Leaves Applied</div>
+            <div>Leaves of  <?php 
+            
+                                $timestamp = mktime(0, 0, 0, date('m'), 10);
+                                $monthName = date("F", $timestamp);
+                                echo $monthName; 
+                            ?>
+            </div>
             <div><?php echo $leaves_applied[0][0]; ?></div>
         </a>
     </div>
