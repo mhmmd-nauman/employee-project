67dd55484793d4a96968cd941f9a511f077961f5
Sobia <Sobia@MALIK.Home>
2015-12-24 22:27:03 +0500

    Report

--- employee/emp_reports_print.php
+++ employee/emp_reports_print.php
@@ -1,33 +1,42 @@
 <?php 
 include (dirname(__FILE__).'/../lib/include.php');
-//include (dirname(__FILE__).'/../lib/header.php'); 
 $objTransaction =new Transaction();
 $objEmployee =new Employee();
-$employee_list=$objEmployee->GetAllEmployee("1 order by emp_file",array("*"));
+$employee_list=$objEmployee->GetAllEmployee("1 order by emp_name",array("*"));
 if($_REQUEST['date']) 
     $date=date('Y-m-d',strtotime($_REQUEST['date']));
 else 
     $date=date('Y-m-d');
 ?>
+<script type="text/javascript">
+window.print()
+</script>
 
-  
-<div class="row">
-    <div class="box col-md-12">
-        <div class="box-inner">
-                <h2><i class="glyphicon glyphicon-star-empty"></i> Report of <?php echo $date; ?></h2>
+    <link href="<?php echo SITE_ADDRESS; ?>css/bootstrap-cerulean.min.css" rel="stylesheet">
+    <link href="<?php echo SITE_ADDRESS; ?>css/charisma-app.css" rel="stylesheet">
+    <link href='<?php echo SITE_ADDRESS; ?>bower_components/responsive-tables/responsive-tables.css' rel='stylesheet'>
+    <link href='<?php echo SITE_ADDRESS; ?>bower_components/bootstrap-tour/build/css/bootstrap-tour.min.css' rel='stylesheet'>
+    <link href='<?php echo SITE_ADDRESS; ?>css/noty_theme_default.css' rel='stylesheet'>
+
     <div class="box-content">    
-        
-        <table style=" font-size: 12px ; width: 100%" cellpadding="7" cellspacong="0" border="1">
+
+<table width="100%" >
+        <tr>
+        <th><b style=" font-size: 18px">Report Date :<?php echo $date;?></b></th>
+        <td  style=" text-align: right;">         <img alt="Employee Logo" src="<?php echo SITE_ADDRESS; ?>img/logo20.png" height="50px" width="200px" ></td>
+        </tr>
+</table><br>
+<table class="table table-striped table-bordered bootstrap-datatable datatable responsive" style=" font-size: 12px">
         <thead>
         <tr>
-            <th style=" width:5% ">Ficha</th>
-            <th style=" width:35% ">Nombre</th>
-            <th style=" width:10% ">Department</th>
-            <th style=" width:10% ">RUT</th>
-            <th style=" width:10% ">FECHA INGRESO</th>
-            <th style=" width:10% ">Today Balance</th>
-            <th style=" width:10% ">FERIADO LEGAL</th>
-            <th style=" width:10% ">DIAS PROGRESIVOS</th>
+            <th>Ficha</th>
+            <th>Nombre</th>
+            <th>Department</th>
+            <th>RUT</th>
+            <th>FECHA INGRESO</th>
+            <th>Today Balance</th>
+            <th>FERIADO LEGAL</th>
+            <th>DIAS PROGRESIVOS</th>
         </tr>
         </thead>
     <tbody>
@@ -35,29 +44,18 @@ else
             $balance_detail= $objTransaction->GetEmpBalanceDetail($employee['emp_id']." and date <= '".$date." 12:60:60'");
             $balance=($balance_detail['I']-$balance_detail['leavesI'])+($balance_detail['D']-$balance_detail['leavesD']);?>      
         <tr>
-            <td style=" text-align:center;"><?php echo $employee['emp_file']; ?></td>
+            <td><?php echo $employee['emp_file']; ?></td>
             <td><?php echo $employee['emp_name']; ?></td>
             <td><?php echo $employee['emp_department']; ?></td>
-            <td style=" text-align:center;"><?php echo $employee['emp_cellnum'];?></td>
-            <td style=" text-align:center;"><?php echo date("m/d/Y",strtotime($employee['emp_current_contract'])); ?></td>
-            <td style=" text-align:right;"><?php   echo number_format($balance, 2);?></td>
-            <td style=" text-align:right;"><?php   echo number_format(($balance_detail['I']-$balance_detail['leavesI']), 2);?></td>
-            <td style=" text-align:right;"><?php   echo number_format(($balance_detail['D']-$balance_detail['leavesD']), 2);?></td>
+            <td><?php echo $employee['emp_cellnum'];?></td>
+            <td><?php echo date("m/d/Y",strtotime($employee['emp_current_contract'])); ?></td>
+            <td><?php   echo number_format($balance, 2);?></td>
+            <td><?php   echo number_format(($balance_detail['I']-$balance_detail['leavesI']), 2);?></td>
+            <td><?php   echo number_format(($balance_detail['D']-$balance_detail['leavesD']), 2);?></td>
         </tr>
         <?php 
         $balance = 0;
            } ?>
     </tbody>
     </table>
-            </div>
-        </div>
     </div>
-</div>
-<?php //include('../lib/footer.php'); ?>
-<script>
-$(function() {
-$('#date').datepicker({
-            dateFormat: 'yy-mm-dd'
-        });
-});
-</script>
